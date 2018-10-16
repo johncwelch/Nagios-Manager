55e57a8dd6702aa6646b0256c83b35517632c074
johncwelch <jwelch@bynkii.com>
2018-10-16 12:53:34 -0400

    Delete Nagios Manager_vers.d

--- build/Nagios Manager.build/Debug/Nagios Manager.build/Objects-normal/x86_64/Nagios Manager_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jwelch/OneDrive\ -\ Florida\ State\ University/NWRDC/Scripts/ASOC\ Projects/Nagios\ Manager/build/Nagios\ Manager.build/Debug/Nagios\ Manager.build/DerivedSources/Nagios\ Manager_vers.c
